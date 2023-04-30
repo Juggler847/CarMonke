c3a37b1ce1b4558d8fc01c87e43944950bc9b8e0
aliencheese <132181321+aliencheesee@users.noreply.github.com>
2023-04-30 00:46:22 -0400

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,55 @@
+﻿using BepInEx;
+using GorillaLocomotion;
+using Oculus.Platform.Models;
+using Photon.Pun;
+using System;
+using System.ComponentModel;
+using System.Net;
+using System.Text;
+using UnityEngine;
+using UnityEngine.XR;
+using Utilla;
+
+namespace DinosauceCars
+{
+    [Description("HauntedModMenu")]
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        bool inRoom;
+
+        void Update()
+        {
+            if(inRoom || !PhotonNetwork.InRoom)
+            {
+                Rigidbody rb = Player.Instance.gameObject.GetComponent<Rigidbody>(); 
+                Vector2 left;
+                GameObject gameObject = GameObject.Find("gorillachestface");
+                InputDevices.GetDeviceAtXRNode(XRNode.LeftHand).TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxis, out left);
+                Vector3 vector = (gameObject.transform.forward * left.y * 5) + (gameObject.transform.right * left.x * 5);
+                bool moving = (vector.x > 0.1f || vector.x < -0.1f) || (vector.y > 0.1f || vector.y < -0.1f);
+
+                if (moving)
+                {
+                    vector.y = rb.velocity.y;
+                    rb.velocity = vector * Player.Instance.scale;
+                }
+            }
+        }
+
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            inRoom = true;
+        }
+
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            inRoom = false;
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace DinosauceCars
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.dinosauce.cars";
+        public const string Name = "DinosauceCars";
+        public const string Version = "1.0.0";
+    }
+}
